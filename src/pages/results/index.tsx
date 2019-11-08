d31652425815d16a9a3c4964d19551d193c2c5c9
Nicolás Silva <sovietfrance@gmail.com>
2019-11-08 00:25:50 -0300

    fix: update styles

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,12 +1,69 @@
 import React from "react";
-import { Input, Logo } from "../../common";
+import styled from "styled-components";
+import { Sidebar, Result } from "./components";
+import { Logo, Input } from "../../common";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2em;
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 1152px;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+  }
+`;
 
 const Results: React.FC = () => {
   return (
-    <div className="layout" style={{ maxWidth: "750px", padding: "1em" }}>
-      <Logo />
-      <Input />
-    </div>
+    <>
+      <Container>
+        <div className="row">
+          <div className="column" style={{ maxWidth: "300px" }}>
+            <Logo />
+          </div>
+          <div
+            className="column"
+            style={{ maxWidth: "750px", paddingLeft: "50px" }}
+          >
+            <Input />
+          </div>
+        </div>
+      </Container>
+
+      <Container>
+        <div className="row">
+          <div className="column" style={{ maxWidth: "300px" }}>
+            <Sidebar />
+          </div>
+          <div
+            className="column"
+            style={{ maxWidth: "750px", paddingLeft: "50px" }}
+          >
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+            <Result />
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
